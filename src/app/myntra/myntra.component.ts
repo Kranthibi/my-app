92ac866e6c7e5ca26b7fa9603aac1b5cd22618e8
Kranthibi <Kranthibi049@gmail.com>
2024-07-29 14:25:05 +0530

    myntra api completed

--- src/app/myntra/myntra.component.ts
+++ src/app/myntra/myntra.component.ts
@@ -25,5 +25,48 @@ export class MyntraComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  mens(){
+    this.products = this.products.filter((product:any)=>product.category.includes("men's clothing"));
 
+  }
+  jewelery(){
+    this.products=this.products.filter((product:any)=>product.category.includes("jewelery"));
+  }
+  electronics(){
+    this.products=this.products.filter((product:any)=>product.category.includes("electronics"));
+  }
+  womens(){
+    this.products=this.products.filter((product:any)=>product.category.includes("women's clothing"));
+  }
+
+  s7to50(){
+    this.products=this.products.filter((product:any)=>product.price >=7 && product.price<=50);
+  }
+
+  f50to300(){
+    this.products=this.products.filter((product:any)=>product.price >=50 && product.price<=300);
+  }
+  t300to600(){
+    this.products=this.products.filter((product:any)=>product.price >=300 && product.price<=600);
+  }
+
+  r600to1000(){
+    this.products=this.products.filter((product:any)=>product.price >=600 && product.price<=1000);
+  }
+  sort(event:any){
+    let value = event.target.value;
+    if(value==="ltoh"){
+      this.products=this.products.sort((a:any,b:any)=>a.price-b.price);
+    }
+    else if(value==="htol"){
+      this.products=this.products.sort((a:any,b:any)=>b.price-a.price);
+    }
+    else{
+      this._myntraService.getproducts().subscribe(
+        (data:any)=>{
+          this.products= data;
+        }
+      )
+    }
+  }
 }
