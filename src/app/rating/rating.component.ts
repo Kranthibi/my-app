de3538e85ce51e270f2e070cdea6ee35dd858158
Kranthibi <Kranthibi049@gmail.com>
2024-07-31 11:09:05 +0530

    sibling cummunication completed

---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit {
+  @Input() public rating:any=0;
+  // @Output() public star:EventEmitter<any>=new EventEmitter;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // add(){
+  //   this.star.emit(this.rating);
+  // }
+
+
+  
+
+
+}
